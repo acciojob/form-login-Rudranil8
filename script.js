3f379a81dd95c4d9e9febcd38b01c9d8432976ee
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 01:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
+	let form = document.querySelector("#myForm");
+	form.addEventListener('submit', getFormValue);
 	event.preventDefault();
     let firstName = document.querySelector("#firstName").value;
     let lastName = document.querySelector("#lastName").value;
