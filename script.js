bbfaad602565fdcdfab0f9b1576b0107b1f89f8c
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 01:12:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getFormvalue() {
 	let form = document.querySelector("#myForm");
-	form.addEventListener('submit', getFormValue);
-	event.preventDefault();
-    let firstName = document.querySelector("#firstName").value;
-    let lastName = document.querySelector("#lastName").value;
-
-    alert(firstName + " " + lastName);
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let firstName = document.querySelector("#firstName").value;
+        let lastName = document.querySelector("#lastName").value;
+        alert(firstName + " " + lastName);
+    });
 }
